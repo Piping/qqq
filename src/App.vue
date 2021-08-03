bdf2a5c16ae5a45c7fa50b8c7fbaf5565824454e
robin <dequanzh@usc.edu>
2021-08-03 00:00:39 -0700

    add electron container + scrollbar fix

--- src/App.vue
+++ src/App.vue
@@ -8,3 +8,25 @@ export default defineComponent({
   name: 'App'
 })
 </script>
+<style>
+/* Works on Firefox */
+* {
+  scrollbar-width: thin;
+  scrollbar-color: blue orange;
+}
+
+/* Works on Chrome, Edge, and Safari */
+*::-webkit-scrollbar {
+  width: 3px;
+}
+
+*::-webkit-scrollbar-track {
+  background: auto;
+}
+
+*::-webkit-scrollbar-thumb {
+  background-color: blue;
+  border-radius: 3px;
+  border: 3px solid white;
+}
+</style>
